e3a6bbf6c95e95ecd72e5e2037d7bb46e64ff593
Bedirhan Caldir <bedirhancaldir@Bedirhans-MacBook-Pro.local>
2014-10-09 18:59:34 +0300

    Player and HighScores class were filled

--- game/src/highscores/HighScores.java
+++ game/src/highscores/HighScores.java
@@ -1,5 +1,68 @@
 package highscores;
 
-public class HighScores {
+import java.util.ArrayList;
 
+/**
+ * This class keeps the first 5 highscores
+ * @author bedirhancaldir
+ */
+public class HighScores {
+	ArrayList<Player> playerList;
+	final int UPPERBOUND = 5;
+
+	/**
+	 * The constructor of the HighScores class. HighScores holds the players having the highest scores up to 5 players.
+	 */
+	public HighScores(){
+		
+		playerList = new ArrayList<Player>();
+	}
+
+	/**
+	 * This method adds the given player to the list with respect to his/her score.
+	 * @param player The player wanted to be added to the list
+	 */
+	public void add(Player player){
+
+		for (int i=0; i<playerList.size(); i++){
+			if (player.getScore() > playerList.get(i).getScore()){ // To hold the order of the ranking
+				playerList.add(i, player); // Addition of the player since it satisfies the conditions
+				if (playerList.size() > UPPERBOUND){ // To remove the players ranked more than 5th.
+					playerList.remove(UPPERBOUND);
+				}
+				break; // To stop the further addition of the same player.
+			}
+		}
+
+		if (playerList.isEmpty()) // If the list is empty, then add directly
+			playerList.add(player);
+		else if (playerList.size() < 5) // If the list has less than 5 players and all of them have higher scores than the new player, 
+			// then the new player will be added to the end of the list since the for loop doesn't add.
+			playerList.add(playerList.size(), player);
+	}
+
+	/**
+	 * This method returns the player found in the list at the given index.
+	 * @param index must be in [1,5] (i.e. getPlayer(3) for the player having 3rd highest score)
+	 * @return The player corresponding to the index given (null if the index isn't in the interval of [1,5])
+	 */
+	public Player getPlayer(int index){
+		
+		if (index <= UPPERBOUND && index > 0) // Check whether the given index is in the permitted range.
+			return playerList.get(index-1);	
+
+		return null; // If not, return null.
+	}
+
+	/**
+	 * 
+	 * @return 
+	 */
+	public String toString(){
+		return null;
+
+	}
 }
+
+
+
--- game/src/highscores/Player.java
+++ game/src/highscores/Player.java
@@ -1,5 +1,98 @@
 package highscores;
 
-public class Player {
+import java.util.Date;
 
+/**
+ * This class generates a player with his/her name, score and game playing date
+ * @author bedirhancaldir
+ */
+public class Player {
+	String name;
+	int score;
+	Date date;
+
+	/**
+	 * The constructor of the Player class.
+	 * @param name The name of the player (must have at most 20 characters)
+	 * @param score The score of the player (must be greater than or equal to 0)
+	 * @param date The date of the game played
+	 */
+	public Player(String name, int score, Date date){
+		setName(name);
+		setScore(score);
+		setDate(date);
+	}
+
+	/**
+	 * The constructor of the Player class. Date will be the running date.
+	 * @param name The name of the player (must have at most 20 characters)
+	 * @param score The score of the player (must be greater than or equal to 0)
+	 */
+	public Player(String name, int score){
+		this(name, score, new Date()); // new Date() -> gives the instance of calling
+	}
+
+	/**
+	 * The constructor of the Player class. The score will be 0.  Date will be the running date. 
+	 * @param name The name of the player (must have at most 20 characters)
+	 */
+	public Player(String name){
+		this(name, 0);
+	}
+
+	/**
+	 * This method sets the name of the player to the given name. Name must be no longer than 20 characters.
+	 * Otherwise it would be cut to its first 20 letters.
+	 * @param name The name of the player
+	 */
+	public void setName(String name){
+		if (name.length() <= 20) 
+			this.name = name;
+		else
+			this.name = name.substring(0,20); // If the length of the name exceeds 20, then the first 20 letters will be taken.
+	}
+
+	/**
+	 * This method sets the date of the game played by the player to the given date.
+	 * @param date The date of the game played
+	 */
+	public void setDate(Date date){
+		this.date = date;
+	}
+
+	/**
+	 * This method sets the score of the player to the given value. The score must be greater than or equal to 0.
+	 * Otherwise the score would be set to 0 automatically.
+	 * @param score The score of the player
+	 */
+	public void setScore(int score){
+		if (score >= 0)
+			this.score = score;
+		else // If score is below 0, then it will be equalized to 0.
+			this.score = 0;
+	}
+
+	/**
+	 * This method returns the name of the player
+	 * @return The name of the player
+	 */
+	public String getName(){
+		return name;
+	}
+
+	/**
+	 * This method returns the date at which this player played the tetris/triris
+	 * @return The date of the particular game played by this player
+	 */
+	public Date getDate(){
+		return date;
+	}
+
+	/**
+	 * This method returns the score of the player
+	 * @return The score of the player
+	 */
+	public int getScore(){
+		return score;
+	}
 }
